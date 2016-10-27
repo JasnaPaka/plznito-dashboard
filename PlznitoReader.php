489012d84f542ade992cc4516ffd5c0000b32a28
Pavel Cvrček <jasnapaka@jasnapaka.com>
2016-10-27 10:40:43 +0200

    Úvodní verze dashboardu

---
+++ PlznitoReader.php
@@ -0,0 +1,159 @@
+<?php
+
+class PlznitoReader
+{
+	const LIST_URL = "http://plznito.cz/api/1.0/tickets/list";
+
+	private $error = false;
+	private $data;
+
+	function __construct()
+	{
+		$this->loadList();
+	}
+
+	private function loadList() {
+		$str = file_get_contents(self::LIST_URL);
+		if ($str === FALSE) {
+			$this->error = true;
+			return;
+		}
+
+		$this->data = json_decode($str);
+		if ($this->data == null) {
+			$this->error = true;
+			return;
+		}
+
+		usort($this->data->items, function($a, $b)
+		{
+			if ($a->edited == null && $b->edited == null) {
+				return strcmp($b->name, $a->name);
+			}
+
+			if ($a->edited == null && $b->edited != null) {
+				return -1;
+			}
+
+			if ($a->edited != null && $b->edited == null) {
+				return 1;
+			}
+
+			$date1 = strtotime ($a->edited->date);
+			$date2 = strtotime ($b->edited->date);
+
+			if ($date1 == $date2) {
+				return 0;
+			}
+
+			return $date1 > $date2 ? -1 : 1;
+		});
+	}
+
+	public function getError() {
+		return $this->error;
+	}
+
+	public function getCount() {
+		return $this->data->count;
+	}
+
+	public function getItems() {
+		return $this->data->items;
+	}
+
+	public function getCategoryName($id) {
+		switch ($id) {
+			case 1:
+				return "Pískoviště, hřiště, sportoviště";
+			case 2:
+				return "Jiné";
+			case 3:
+				return "Silnice, cyklostezky";
+			case 4:
+				return "Odpadky, černá skládka";
+			case 5:
+				return "Chodník";
+			case 6:
+				return "Kanalizace";
+			case 7:
+				return "Dopravní značení";
+			case 8:
+				return "Lavičky, zábradlí";
+			case 9:
+				return "Veřejné osvětlení";
+			case 10:
+				return "Veřejná zeleň";
+			case 11:
+				return "Autovrak";
+			case 12:
+				return "Zastávky MHD";
+			default:
+				return "(neznámá - ".$id.")";
+		}
+	}
+
+	public function getStatusName($id) {
+		switch ($id) {
+			case 1:
+				return "Nové";
+			case 2:
+				return "V řešení";
+			case 3:
+				return "Vyřešené";
+			case 4:
+				return "Odmítnuto";
+			case 5:
+				return "Nepatří";
+			case 6:
+				return "Zodpovězeno";
+			default:
+				return "(neznámý - ".$id.")";
+		}
+	}
+
+	public function getStatusBackground($id) {
+		switch ($id) {
+			case 1:
+				return "#4986e7";
+			case 2:
+				return "#ff7537";
+			case 3:
+				return "#16a765";
+			case 4:
+				return "#fb4c2f";
+			case 5:
+				return "#fb4c2f";
+			case 6:
+				return "#16a765";
+			default:
+				return "white";
+		}
+	}
+
+	public function getReportPerson($description) {
+
+		// Tomáš Benda
+		if (strpos($description, "TB") !== false || strpos($description, "/tb") !== false) {
+			return '<abbr title="Tomáš Benda">TB</abbr>';
+		}
+
+		// Tomáš Halada
+		if (strpos($description, "TH") !== false) {
+			return '<abbr title="Tomáš Halada">TH</abbr>';
+		}
+
+		// Pavel Cvrček
+		if (strpos($description, "PC") !== false) {
+			return '<abbr title="Pavel Cvrček">PC</abbr>';
+		}
+
+		// Eva Haunerová
+		if (strpos($description, "EH") !== false) {
+			return '<abbr title="Eva Haunerová">EH</abbr>';
+		}
+
+		return "";
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<title>Plznito Dashboard</title>
+
+	<link href="./css/style.css" rel="stylesheet" />
+</head>
+<body>
+
+<h1>Plznito Dashboard</h1>
+
+<p>Neoficiální přehled hlášení zadaných do aplikace <a href="http://plznito.cz/">Plznito</a>. Jednotlivá hlášení
+jsou řazena podle data poslední úpravy, aby bylo možné sledovat, jak se s jednotlivými hlášeními na městě pracuje.
+Připomínky? Náměty? <a href="mailto:jasnapaka@jasnapaka.com">Pište</a>.</p>
+
+<?php
+	include "PlznitoReader.php";
+	$reader = new PlznitoReader();
+
+	if ($reader->getError()) {
+?>
+
+<div id="error">
+
+</div>
+
+<?php
+	} else  {
+?>
+
+<p><strong>Počet hlášení</strong>: <?php print($reader->getCount()) ?></p>
+
+<?php
+		if ($reader->getCount() > 0) {
+			print('<table><col width="5%" /><col width="35%" /><col width="25%" /><col width="15%" /><col width="15%" /><col width="5%" />');
+			print('<thead><tr><th>Id</th><th>Název</th><th>Kategorie</th><th>Stav</th><th>Poslední úprava</th>
+				<th><abbr title="Iniciály některých osob, které si podepisují svá hlášení do aplikace.">Inic.</abbr></th></tr></thead><tbody>');
+
+			foreach ($reader->getItems() as $item) {
+				print ('<tr>');
+				printf ('<td>%s</td>', $item->id);
+				printf ('<td><a href="http://plznito.cz/map#!/activity/%d">%s</a></td>', $item->id, $item->name);
+				printf ('<td>%s</td>', $reader->getCategoryName($item->category_id));
+				printf ('<td><span class="item-status" style="background-color: %s">%s</span></td>', $reader->getStatusBackground($item->status_id), $reader->getStatusName($item->status_id));
+				printf ('<td>%s</td>', $item->edited != null ? date('d. m. Y H:i', strtotime($item->edited->date)) : "(neuvedeno)");
+				printf ('<td>%s</td>', $reader->getReportPerson($item->description));
+				print ('</tr>');
+			}
+
+			print('</tbody></table>');
+		}
+	}
+?>
+
+</body>
+</html>
